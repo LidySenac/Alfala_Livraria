35beb75fd0865a89dbc503af15a1d5ad0ea9e9ed
LidySenac <lidian.poliveira@senacsp.edu.br>
2024-06-05 14:39:00 -0300

    Header-2

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,17 +1,26 @@
 
 import logo from '../../img/logo.svg'
-import './style.css'
+import styled from 'styled-components';
+
+const Logos = styled.div`
+display: flex;
+font-size: 30px;
+`
+
+const Imagem = styled.img`
+margin-right: 10px;
+`
 
 function Logo() {
     return (
-        <div className='logo'>
-            <img 
+        <Logos>
+            <Imagem 
             src={logo} 
             alt='Alfala logo'
             className='logo-img'>
-            </img>
+            </Imagem>
             <p><strong>Alfala</strong>Books</p>
-        </div>
+        </Logos>
     )
 }
 
